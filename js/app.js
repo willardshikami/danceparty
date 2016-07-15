8c9dc0773dc55c86ac9377c50ba4453e653b3b28
willardshikami <willardshikami@gmail.com>
2016-07-15 15:35:00 +0300

    added moving objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 $(document).foundation()
+$(function () {
+  
+  function circleBlink () {
+    var circles = $('.circles');
+      circles.hide();
+      $(circles[0]).fadeIn('slow', function () {
+        $(circles[1]).fadeIn('slow', function () {
+          $(circles[2]).fadeIn('slow', function () {
+           $(circles[3]).fadeIn('slow', function () {
+             $(circles[4]).fadeIn('slow', function () {
+               $(circles[5]).fadeIn('slow');
+               });
+             });
+            });
+          });
+        });  
+  	};
+  setInterval(circleBlink, 3500);
+})
